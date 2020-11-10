b19956c855838da4659a4f58f3dc0b895897317e
Godiya <godiyazambwa@gmail.com>
2020-11-10 15:54:41 +0000

    Godiya-added env file

--- models/config.js
+++ models/config.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
-    db: 'blog_project',
-    user:'root',
-    password:'',
-    host:'localhost',
-    dialect:'mysql'
+    db: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
 }
