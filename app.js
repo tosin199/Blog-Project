f8ee138811478efec9837d5e5db5c2e45ba02c06
Simeon <olabs06@yahoo.com>
2020-12-08 12:00:26 +0100

    modifying files

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const db = require('./models');
 
 var app = express();
 
-db.sequelize.sync({alter:true }); //force:false
+db.sequelize.sync({alter:false}); //force:false
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
