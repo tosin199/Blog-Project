0b2c298e13fae20e2703d1c45f2f7d027ec46975
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2020-12-19 14:56:51 +0100

    mailjet

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,7 +6,8 @@ const multer = require('multer');
 const helpers = require('../config/helper')
 const multerConfig = require('../config/multer');
 require('dotenv').config()
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
+const mailjet = require('node-mailjet');
 
 async function  getUser(req,res){
   const user = await models.user.findOne({where:{id:req.user.id},attributes:['firstname','lastname','profilePicture']})
@@ -147,47 +148,31 @@ async function sendCode(req,res){
     let value,val;
     value = Math.floor(1000 + Math.random() * 9000000);
     val = value.toString();
-
-    // Generate SMTP service account from ethereal.email
-    nodemailer.createTestAccount((err, account) => {
-      if (err) {
-          console.error('Failed to create a testing account. ' + err.message);
-          return process.exit(1);
-      }
-
-      console.log('Credentials obtained, sending message...');
-
-      // Create a SMTP transporter object
-      let transporter = nodemailer.createTransport({
-          host: account.smtp.host,
-          port: account.smtp.port,
-          secure: account.smtp.secure,
-          auth: {
-              user: account.user,
-              pass: account.pass
-          }
-      });
-
-      // Message object
-      let message = {
-          from: '24/7 News <CodeVerification@24/7.com>',
-          to: email,
-          subject: 'Password Reset Code ✔',
-          text: val,
-          // html: '<p><b>Hello</b> to myself!</p>'
-      };
-
-      transporter.sendMail(message, (err, info) => {
-          if (err) {
-              console.log('Error occurred. ' + err.message);
-              return process.exit(1);
-          }
-
-          console.log('Message sent: %s', info.messageId);
-          // Preview only available when sending through an Ethereal account
-          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-      });
-    });
+    
+    mailjet.connect(
+      process.env.MJ_APIKEY_PUBLIC,
+      process.env.MJ_APIKEY_PRIVATE
+    );
+    const request = await mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: 'ask4ismailsadiq@gmail.com',
+            Name: 'IDrip',
+          },
+          To: [
+            {
+              Email: email,
+              Name: User.firstname+' '+User.lastname,
+            },
+          ],
+          Subject: 'Password Reset Code ✔',
+          TextPart: val,
+          // HTMLPart:
+        },
+      ],
+    })
+    console.log(result.body);
     await models.resetPasswordCode.create({code:val,userId:User.id});
     res.json({'msg':'code sent'});
   }else{res.json({'msg':'No account with this email'})}
